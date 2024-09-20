c9c64b459ca7648f6f6df06a2508d53175cdaff1
Mohamed Irfan <irfanmm96@gmail.com>
2024-09-20 21:25:25 +0530

    Work in progress

--- resources/views/livewire/add-post-tags.blade.php
+++ resources/views/livewire/add-post-tags.blade.php
@@ -1,19 +1,28 @@
-<div>
-    You are going to add tags to post #{{ $post->id }}
-
-    <select wire:model.live="post.tags" multiple>
-
-        <option value="tag-1">Tag 1</option>
-
-        <option value="tag-2">Tag 2</option>
-
-        <option value="tag-3">Tag 3</option>
-
-    </select>
-
+<div style="display: grid;place-items: center;height: 100vh">
 
     <div>
-        Selected tags : {{ collect($post->tags ?? ['none'])->join(', ') }}
+        <div>
+            You are going to add tags to post #{{ $post->id }}
+        </div>
+
+        <br>
+
+        <select wire:model.live="post.tags" multiple>
+
+            <option value="tag-1">Tag 1</option>
+
+            <option value="tag-2">Tag 2</option>
+
+            <option value="tag-3">Tag 3</option>
+
+        </select>
+
+
+        <br>
+
+        <div style="margin-top: 10px">
+            Selected tags : {{ collect($post->tags ?? ['-'])->join(', ') }}
+        </div>
     </div>
 
 </div>
